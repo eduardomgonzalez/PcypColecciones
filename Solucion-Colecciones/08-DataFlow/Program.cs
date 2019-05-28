79b53e88a7147b376546601aadee9cedd07cc15e
eduardomgonzalez <44064190+eduardomgonzalez@users.noreply.github.com>
2019-05-28 11:45:40 -0300

    Agrego ejercicio con DataFlow

---
+++ Solucion-Colecciones/08-DataFlow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjercicioDataFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var TaskA = Task.Factory.StartNew<int>(() =>
+            {
+                Console.WriteLine("Tarea A en proceso. Retorno 10.");
+                return 10;
+            });
+
+            var TaskB = Task.Factory.StartNew<int>(() =>
+            {
+                Console.WriteLine("Tarea B en proceso. Retorno 20.");
+                return 20;
+            });
+
+            // Ejecuto una tercer tarea, pero antes espero que se complenten las anteriores
+            Task TaskC = Task.Factory.ContinueWhenAll(new Task<int>[] { TaskA, TaskB },
+                (tasks) =>
+
+                Console.WriteLine("Suma total de retornos de A + C = {0}", tasks[0].Result + tasks[1].Result)
+
+                );
+
+            TaskC.Wait();
+            Console.WriteLine("Presione una tecla...");
+            Console.ReadKey();
+        }
+
+    }
+}
